e9d5eb3f19a30febbd0b706e23ead6e80ac165e2
Nesvetailov Artem <75523744+Dibrit@users.noreply.github.com>
2026-04-03 13:18:28 +0500

    Add files via upload

---
+++ lab9/shop_back/api/views.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    @action(detail=True, methods=['get'])
+    def products(self, request, pk=None):
+        category = self.get_object()
+        products = Product.objects.filter(category=category)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    @action(detail=False, methods=['delete']) 
+    def delete_negative_prices(self, request, pk=None): 
+        products = Product.objects.filter(price__lt=0).delete() 
+        return Response({"deleted": products})
